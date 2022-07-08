2b6263f2cbe876e8235dedaf421fef4f62a62c0a
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-07-08 20:10:22 +0200

    Initial Commit

---
+++ chatty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatty
+//
+//  Created by Ing. Ebu Celik on 08.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
