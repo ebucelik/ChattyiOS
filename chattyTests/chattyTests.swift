0c1b0b1793702b10d9c090afb45f73dc93d6a1cd
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-07-08 22:43:08 +0200

    Added some tests

--- chattyTests/chattyTests.swift
+++ chattyTests/chattyTests.swift
@@ -18,19 +18,22 @@ class chattyTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testCalculation() throws {
+        let a = 10
+        let b = 17
+
+        let result = a + b
+
+        XCTAssertTrue(result == 27)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCalculationFailure() throws {
+        let a = 10
+        let b = 8
+
+        let result = a + b
+
+        XCTAssertTrue(result == 1)
     }
 
 }
