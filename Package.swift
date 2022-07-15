8dea325b8abcb8ca8f236e7d5b4f5c0ddb0d7351
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-07-15 16:26:13 +0200

    Use version in swifthelper dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "0.9.0"),
-        .package(url: "https://gitlab.com/ebucelik/swifthelper.git")
+        .package(url: "https://gitlab.com/ebucelik/swifthelper.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
