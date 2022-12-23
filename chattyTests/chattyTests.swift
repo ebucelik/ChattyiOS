6e0779ace85b11791652d9f2fa3103a65f31e4e5
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-12-23 19:11:34 +0100

    get swift helper library from github

--- chattyTests/chattyTests.swift
+++ chattyTests/chattyTests.swift
@@ -8,32 +8,4 @@
 import XCTest
 @testable import chatty
 
-class chattyTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testCalculation() throws {
-        let a = 10
-        let b = 17
-
-        let result = a + b
-
-        XCTAssertTrue(result == 27)
-    }
-
-    func testCalculationFailure() throws {
-        let a = 10
-        let b = 8
-
-        let result = a + b
-
-        XCTAssertTrue(result != 1)
-    }
-
-}
+class chattyTests: XCTestCase { }
